e022eb6b60f4289117ceed8d71eed542e5d2bd95
SWAUGAT-BEURA <cse.190310300@silicon.ac.in>
2021-12-05 20:25:44 +0530

    adding script for dbconfig

--- db/db.config.js
+++ db/db.config.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
 dotenv.config();
 const config={
-    // uri:`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.s7i2h.mongodb.net/${process.env.DB_DATABASENAME}?retryWrites=true&w=majority`
-    uri:`mongodb+srv://SWAGgd132001:SWAGgd132001@cluster0.popjg.mongodb.net/contactsManagerDatabase?retryWrites=true&w=majority`
+    uri:`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.s7i2h.mongodb.net/${process.env.DB_DATABASENAME}?retryWrites=true&w=majority`
+    // uri:`mongodb+srv://SWAGgd132001:SWAGgd132001@cluster0.popjg.mongodb.net/contactsManagerDatabase?retryWrites=true&w=majority`
 }
 module.exports=config;
