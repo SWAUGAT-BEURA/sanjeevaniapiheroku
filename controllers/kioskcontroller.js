91d2385a1d40dd08d83dbb8e911a927147944edc
SWAUGAT-BEURA <cse.190310300@silicon.ac.in>
2022-02-12 19:08:33 +0530

    kiosk added api

---
+++ controllers/kioskcontroller.js
@@ -0,0 +1,49 @@
+const KioskUser = require("../models/kioskuser"); 
+
+
+exports.getAllAdState = (req, res) => {
+    Ads.findById(req.params.advertisementId)
+    .then((course) => {
+        if(course!= null) {
+            res.statusCode = 200;
+			res.setHeader('Content-Type', 'application/json');
+			res.json(course.video);
+        } else {
+            res.json({
+                msg: "wasn't able to find the given Advertisement"
+            })
+        }
+    })
+    .catch((err) => {
+        res.json(err)
+    })
+}
+
+
+// handleing post request
+exports.addkioskuser("/kiosk/api/user", async (req, res) => {
+    try {
+        const addingKioskUsers = new KioskUser(req.body);
+        await addingKioskUsers.save();
+        res.send("successfully added");
+    } catch (e) {
+        res.send(e);
+    }
+})
+
+// get request for checking phone number
+app.checkphonenumber("/kiosk/api/user/:phno", async (req, res) => {
+    try {
+        const phno = req.params.phno;
+        console.log(phno);
+        if (!phno) {
+            res.send("phno undefind");
+        } else {
+            const getUser = await KioskUser.findOne({ phno: phno }, 'phno name').exec();
+            console.log(getUser);
+            res.send(getUser);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+})
---
+++ models/kioskuser.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    phno: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type:String
+    },
+    age: {
+        type:Number
+    },
+    gender: {
+        type:String
+    },
+    empDetails: {
+        type:String
+    },
+    score: {
+        type: Number,
+        default : "0"
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+
+})
+
+// new collection
+
+const KioskUser = new mongoose.model("KioskUser", userSchema)
+module.exports = KioskUser
