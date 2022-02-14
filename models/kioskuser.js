3acb3efebaceac2d4b543c045a6093ef57e60e5d
SWAUGAT-BEURA <cse.190310300@silicon.ac.in>
2022-02-14 14:04:36 +0530

    adding the total user api phone no to string

--- models/kioskuser.js
+++ models/kioskuser.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     phno: {
-        type: Number,
+        type: String,
         required: true,
         unique: true
     },
