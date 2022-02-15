f5437fe73f14b71e9d7d6b6de5f9d35e43c2a79a
SWAUGAT-BEURA <cse.190310300@silicon.ac.in>
2022-02-15 17:15:22 +0530

    adding qcpr register api changes

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,18 +1,56 @@
 const User=require('../models/User');
-const bcrypt=require('bcryptjs');
+const bcryt=require('bcryptjs');
 
-exports.userRegister = async(req,res) => {
+// exports.userRegister = async(req,res) => {
+//     try{
+//         const hashedPass=await bcrypt.hash(req.body.password,10);
+//         const newUser=new User({
+//             fullname: req.body.fullname,
+//             email: req.body.email,
+//             password:hashedPass,
+//             phone:req.body.phone
+//         });
+//         const user=await newUser.save();
+//         res.status(200).json(user);
+//     } catch(err){
+//         res.status(500).json(err);
+//     }
+// }
+
+exports.userRegister=async (req,res)=>{
+    const userSchema=joi.object({
+        fullname:joi.string().required().min(4),
+        email:joi.string().required(),
+        phone:joi.string().required().max(10),
+        password:joi.string().required().min(6).max(16)
+    })
+    
     try{
-        const hashedPass=await bcrypt.hash(req.body.password,10);
-        const newUser=new User({
-            fullname: req.body.fullname,
-            email: req.body.email,
-            password:hashedPass,
-            phone:req.body.phone
-        });
-        const user=await newUser.save();
-        res.status(200).json(user);
-    } catch(err){
-        res.status(500).json(err);
+        let userfields= await userSchema.validateAsync(req.body)       
+        let user=await User.findOne({email:userfields.email});
+        if(!user){
+
+            user=new User(userfields)
+            const salt=await bcryt.genSalt(10);
+            user.password=await bcryt.hash(user.password,salt);
+            await user.save();
+            res.status(200).json({
+                message:"user registered successfully",
+                userData:user
+            })
+        }else{
+            res.status(400).json({
+                message:"user already eists with the email id",
+            
+            })
+        }
+
+    }catch(err){
+        res.status(500).json({
+            message:"Something Went Wrong",
+            error:err
+        })
     }
+    
+
 }
