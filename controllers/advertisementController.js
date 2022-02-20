27d930f7b8a0c044adbbe2864adff57fa4d93098
SWAUGAT-BEURA <cse.190310300@silicon.ac.in>
2022-02-20 17:41:08 +0530

    adding kios ad api changes1.7

--- controllers/advertisementController.js
+++ controllers/advertisementController.js
@@ -38,40 +38,40 @@ exports.getSingleAd = (req, res) => {
     })
 }
 exports.getAllAdState=async(req,res)=>{
-    const name1= req.params.state;  
+    // const name1= req.params.state;  
     
-    Advertisement.find({ 'state': name1 }, function (err, docs) {
-        if (err) { return handleError(err) } //handle possible errors
-        console.log(docCount)
+    // Advertisement.find({ 'state': name1 }, function (err, docs) {
+    //     if (err) { return handleError(err) } //handle possible errors
+    //     console.log(docCount)
         
-        res.status(200).json({
-            message:"successfull",
-            ads: docs
-        })
-    });
-    // try{
-    //     let contacts=await Ads.find({state:req.params.state}).populate('state');
-    //     if(!contacts){
-    //         contacts=[]
-    //     }
-    //     const contact=await contacts.findOne({name:name1});
-    //     if(contact){
-    //         res.status(200).json({
-    //             message:"contact fetched",
-    //             contact:contact
-    //         })
-    //     }else{
-    //         res.status(400).json({
-    //             message:"contact not found"
-    //         })
-    //     }
-
-    // }catch(err){
-    //     res.status(500).json({
-    //         message:"something went wrong",
-    //         error:err
+    //     res.status(200).json({
+    //         message:"successfull",
+    //         ads: docs
     //     })
-    // }
+    // });
+    try{
+        let contacts=await Advertisement.find({state:req.params.state}).populate('state');
+        if(!contacts){
+            contacts=[]
+        }
+        const contact=await contacts.findOne({name:name1});
+        if(contact){
+            res.status(200).json({
+                message:"contact fetched",
+                contact:contact
+            })
+        }else{
+            res.status(400).json({
+                message:"contact not found"
+            })
+        }
+
+    }catch(err){
+        res.status(500).json({
+            message:"something went wrong",
+            error:err
+        })
+    }
 }
 
 
