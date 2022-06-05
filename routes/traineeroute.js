30ce87746ca84098783b4f52b5fb792d91e142db
SWAUGAT-BEURA <cse.190310300@silicon.ac.in>
2022-06-05 22:04:41 +0530

    added trainee api1.4

--- routes/traineeroute.js
+++ routes/traineeroute.js
@@ -1,8 +1,17 @@
 const traineeRouter = require("express").Router();
 const traineeController = require("../controllers/traineeController");
 
-traineeController.route("/posttrainee").get(traineeController.getTraineeNumber);
-//   .post(traineeController.addTrainee);
+router.get("/", traineeController.getTraineeNumber);
+
+// router.post('/addContact/:id',auth,contactController.createcontact)
+
+// router.put("/updateContact/:id",auth,contactController.updateContacts)
+
+// router.delete('/deleteContact/:id',auth,contactController.deleteContact)
+
+// router.get("/getbyid/:id",auth,contactController.getContactbyId)
+
+// router.get("/getbyname/:name",auth,contactController.getContactbyName)
 
 module.exports = traineeRouter;
 // .put(traineeController.updatetrainee);
