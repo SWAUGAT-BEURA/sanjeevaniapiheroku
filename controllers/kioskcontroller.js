c7d59630ff8bdff50021d79ae838c18f94666025
SWAUGAT-BEURA <cse.190310300@silicon.ac.in>
2022-06-15 20:30:50 +0530

    merged kiosk and trainee

--- controllers/kioskcontroller.js
+++ controllers/kioskcontroller.js
@@ -1,107 +1,164 @@
-const KioskUser = require("../models/kioskuser"); 
+const KioskUser = require("../models/kioskuser");
+const Trainee = require("../models/traineeModel");
 
-
-exports.getAllAdState = (req, res) => {
-    Ads.findById(req.params.advertisementId)
-    .then((course) => {
-        if(course!= null) {
-            res.statusCode = 200;
-			res.setHeader('Content-Type', 'application/json');
-			res.json(course.video);
-        } else {
-            res.json({
-                msg: "wasn't able to find the given Advertisement"
-            })
-        }
+exports.getTrainee = (req, res) => {
+  Trainee.find({})
+    .then((trainee) => {
+      if (trainee) {
+        res.setHeader("Content-Type", "application/json");
+        res.statusCode = 200;
+        res.json(trainee);
+      } else {
+        res.json({
+          msg: "No courses found",
+        });
+      }
     })
     .catch((err) => {
-        res.json(err)
+      res.json(err);
+    });
+};
+
+exports.createTrainee = (req, res) => {
+  Trainee.create({
+    trainee: req.body.trainee,
+  })
+    .then((trainee) => {
+      if (trainee) {
+        res.setHeader("Content-Type", "application/json");
+        res.statusCode = 200;
+        res.json(trainee);
+      }
     })
-}
+    .catch((err) => {
+      res.json(err);
+    });
+};
+
+exports.updateTrainee = (req, res) => {
+  Trainee.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: { trainee: req.body.trainee },
+    },
+    { new: true }
+  )
+    .then((trainee) => {
+      res.setHeader("Content-Type", "application/json");
+      res.statusCode = 200;
+      res.json(trainee);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+};
+
+//get all the ads list by state
+exports.getAllAdState = (req, res) => {
+  Ads.findById(req.params.advertisementId)
+    .then((course) => {
+      if (course != null) {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(course.video);
+      } else {
+        res.json({
+          msg: "wasn't able to find the given Advertisement",
+        });
+      }
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+};
 
 //count total users
-exports.countuserstotal=async(req,res)=>{
-    totalusers=0;
-    KioskUser.countDocuments({}, function(err, docCount) {
-        if (err) { return handleError(err) } //handle possible errors
-        console.log(docCount)
-        totalusers=docCount
-        res.status(200).json({
-            message:"successfull",
-            totalusers: totalusers
-        })
-        //and do some other fancy stuff
-    })       
-  
-}
+exports.countuserstotal = async (req, res) => {
+  totalusers = 0;
+  KioskUser.countDocuments({}, function (err, docCount) {
+    if (err) {
+      return handleError(err);
+    } //handle possible errors
+    console.log(docCount);
+    totalusers = docCount;
+    res.status(200).json({
+      message: "successfull",
+      totalusers: totalusers,
+    });
+    //and do some other fancy stuff
+  });
+};
 //count total males
-exports.countuserstotalmales=async(req,res)=>{
-    totalusers=0;
-    KioskUser.countDocuments({"gender": "male"}, function(err, docCount) {
-        if (err) { return handleError(err) } //handle possible errors
-        console.log(docCount)
-        totalusers=docCount
-        res.status(200).json({
-            message:"successfull",
-            totalmaleusers: totalusers
-        })
-        //and do some other fancy stuff
-    })       
-  
-}
+exports.countuserstotalmales = async (req, res) => {
+  totalusers = 0;
+  KioskUser.countDocuments({ gender: "male" }, function (err, docCount) {
+    if (err) {
+      return handleError(err);
+    } //handle possible errors
+    console.log(docCount);
+    totalusers = docCount;
+    res.status(200).json({
+      message: "successfull",
+      totalmaleusers: totalusers,
+    });
+    //and do some other fancy stuff
+  });
+};
 
 //count total females
-exports.countuserstotalfemales=async(req,res)=>{
-    totalusers=0;
-    KioskUser.countDocuments({"gender": "female"}, function(err, docCount) {
-        if (err) { return handleError(err) } //handle possible errors
-        console.log(docCount)
-        totalusers=docCount
-        res.status(200).json({
-            message:"successfull",
-            totalfemaleusers: totalusers
-        })
-        //and do some other fancy stuff
-    })       
-  
-}
-
+exports.countuserstotalfemales = async (req, res) => {
+  totalusers = 0;
+  KioskUser.countDocuments({ gender: "female" }, function (err, docCount) {
+    if (err) {
+      return handleError(err);
+    } //handle possible errors
+    console.log(docCount);
+    totalusers = docCount;
+    res.status(200).json({
+      message: "successfull",
+      totalfemaleusers: totalusers,
+    });
+    //and do some other fancy stuff
+  });
+};
 
 // handleing post request
-exports.addkioskuser=async(req,res)=>{
-    try {
-        const addingKioskUsers = new KioskUser(req.body);
-        await addingKioskUsers.save();
-        res.status(200).json({
-            message:"successfully added",
-            kioskuser: addingKioskUsers
-        })
-    } catch (e) {
-        res.send(e);
-    }
-}
+exports.addkioskuser = async (req, res) => {
+  try {
+    const addingKioskUsers = new KioskUser(req.body);
+    await addingKioskUsers.save();
+    res.status(200).json({
+      message: "successfully added",
+      kioskuser: addingKioskUsers,
+    });
+  } catch (e) {
+    res.send(e);
+  }
+};
 
 // get request for checking phone number
-exports.checkphonenumber=async(req,res)=>{
-    try {
-        const phno = req.params.phno;
-        console.log(phno);
-        if (!phno) {
-            res.send("New User");
-        } else {
-            const getUser = await KioskUser.findOne({ phno: phno }, 'phno name').exec();
-            console.log(getUser);
-            // res.send(getUser);
-            res.status(200).json({
-                message:"successfully fetched th details",
-                kioskuser: getUser
-            })
-        }
-    } catch (error) {
-        console.log(error);
+exports.checkphonenumber = async (req, res) => {
+  try {
+    const phno = req.params.phno;
+    console.log(phno);
+    if (!phno) {
+      res.send("New User");
+    } else {
+      const getUser = await KioskUser.findOne(
+        { phno: phno },
+        "phno name"
+      ).exec();
+      console.log(getUser);
+      // res.send(getUser);
+      res.status(200).json({
+        message: "successfully fetched th details",
+        kioskuser: getUser,
+      });
     }
-}
-
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 // exports.getAllScores = (req, res) => {
 //     KioskUser.findById(req.params.userId)
--- routes/kioskroute.js
+++ routes/kioskroute.js
@@ -1,26 +1,24 @@
-const express=require('express');
-const kioskcontroller = require('../controllers/kioskcontroller');
-
-
+const express = require("express");
+const kioskcontroller = require("../controllers/kioskcontroller");
 
 const kioskrouter = express.Router();
 
 //check phonenumber
-kioskrouter.route("/user/:phno")
-.get(kioskcontroller.checkphonenumber);
+kioskrouter.route("/user/:phno").get(kioskcontroller.checkphonenumber);
 
-//kiosk user 
-kioskrouter.route("/user")
-.post(kioskcontroller.addkioskuser);
+//kiosk user
+kioskrouter.route("/user").post(kioskcontroller.addkioskuser);
 
-kioskrouter.route("/totalusers")
-.get(kioskcontroller.countuserstotal);
+kioskrouter.route("/totalusers").get(kioskcontroller.countuserstotal);
 
-kioskrouter.route("/totalmales")
-.get(kioskcontroller.countuserstotalmales);
-kioskrouter.route("/totalfemales")
-.get(kioskcontroller.countuserstotalfemales);
+kioskrouter.route("/totalmales").get(kioskcontroller.countuserstotalmales);
+kioskrouter.route("/totalfemales").get(kioskcontroller.countuserstotalfemales);
 
+kioskrouter.get("/getnumber", kioskcontroller.getTrainee);
+
+kioskrouter.post("/posttrainee", kioskcontroller.createTrainee);
+
+kioskrouter.put("/updatetrainee/:id", kioskcontroller.updateTrainee);
 
 // kioskrouter.route('/:userId/scores')
 // .get(kioskcontroller.getAllScores)
@@ -30,7 +28,5 @@ kioskrouter.route("/totalfemales")
 // 	res.end('PUT operation not supported');
 // })
 // .delete(kioskcontroller.deleteAllScores)
-
-
 
 module.exports = kioskrouter;
